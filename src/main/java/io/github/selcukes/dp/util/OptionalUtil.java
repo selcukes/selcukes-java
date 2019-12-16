a234073c6d9104f12793714b3c78cc4ac0457e34
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2019-12-16 18:44:04 +0530

    Updated OptionalUtil

--- src/main/java/io/github/selcukes/dp/util/OptionalUtil.java
+++ src/main/java/io/github/selcukes/dp/util/OptionalUtil.java
@@ -1,9 +1,28 @@
 package io.github.selcukes.dp.util;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
 public class OptionalUtil {
     public static <T> T unwrap(Optional<T> optional) {
         return optional.orElse(null);
     }
+
+    public static <T> Optional<T> OrElse(Optional<T> optional, T fallbackValue) {
+
+        if (optional.isPresent())
+            return optional;
+        else
+            return Optional.of(fallbackValue);
+    }
+
+    public static <T> Optional<T> unwrap(Optional<T> optional, Supplier<T> fallbackValue) {
+        if (optional.isPresent()) {
+            return optional;
+        }
+
+        return Optional.ofNullable(fallbackValue.get());
+    }
+
+
 }
