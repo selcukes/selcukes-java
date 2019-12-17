38bc1e138fc11790c5197c388a17d9a17be2ffef
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2019-12-17 23:03:22 +0530

    Updated Test class

--- src/test/java/io/github/selcukes/tests/WebDriverBinaryTest.java
+++ src/test/java/io/github/selcukes/tests/WebDriverBinaryTest.java
@@ -4,10 +4,8 @@ import io.github.selcukes.dp.DriverPool;
 
 public class WebDriverBinaryTest {
     public static void main(String[] args) {
-
-        DriverPool.chromeDriver().targetPath("temp").setup();
-        DriverPool.ieDriver().targetPath("temp").setup();
-        DriverPool.firefoxDriver().targetPath("temp").setup();
-
+        DriverPool.chromeDriver().setup();
+        DriverPool.ieDriver().targetPath("temp").arch32().setup();
+        DriverPool.firefoxDriver().targetPath("temp").version("v0.26.0").arch64().setup();
     }
 }
