923093314e3eca1e0da27ed531eed22c29bbc9d6
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2019-12-18 20:07:46 +0530

    Proxy setup

--- src/main/java/io/github/selcukes/dp/util/HttpUtils.java
+++ src/main/java/io/github/selcukes/dp/util/HttpUtils.java
@@ -4,8 +4,8 @@ import io.github.selcukes.dp.exception.DriverPoolException;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
+import java.util.Optional;
 import java.util.function.Function;
 
 public final class HttpUtils {
@@ -20,7 +20,7 @@ public final class HttpUtils {
 
         try {
             URL url = new URL(endpoint);
-            httpURLConnection = (HttpURLConnection) url.openConnection();
+            httpURLConnection = (HttpURLConnection) url.openConnection(OptionalUtil.unwrap(createProxy("https://127.0.0.1:0:8080")));
             httpURLConnection.setDoInput(true);
             httpURLConnection.connect();
             return httpURLConnection;
@@ -48,4 +48,27 @@ public final class HttpUtils {
     private static <T> T extract(HttpURLConnection connection, Function<HttpURLConnection, T> condition) {
         return condition.apply(connection);
     }
+
+    public static Optional<Proxy> createProxy(String proxy) {
+        Optional<URL> url = getProxyUrl(proxy);
+        if (url.isPresent()) {
+            String proxyHost = url.get().getHost();
+            int proxyPort = url.get().getPort() == -1 ? 80
+                    : url.get().getPort();
+            return Optional.of(new Proxy(Proxy.Type.HTTP,
+                    new InetSocketAddress(proxyHost, proxyPort)));
+        }
+        return Optional.empty();
+    }
+
+    public static Optional<URL> getProxyUrl(String proxy) {
+        Optional<URL> proxyUrl = Optional.empty();
+        try {
+            proxyUrl = Optional.of(new URL(proxy));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return proxyUrl;
+    }
+
 }
