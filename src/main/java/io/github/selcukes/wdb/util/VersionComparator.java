cea3226fb280c67c54bd8c07c25a39ee089e4707
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2019-12-25 14:06:08 +0530

    [Fix] Version number sorting issue #17

--- src/main/java/io/github/selcukes/wdb/util/VersionComparator.java
+++ src/main/java/io/github/selcukes/wdb/util/VersionComparator.java
@@ -1,45 +1,77 @@
 package io.github.selcukes.wdb.util;
 
 import java.util.Comparator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-public final class VersionComparator
-    implements Comparator<String> {
-    final Pattern p = Pattern.compile("^\\d+");
-
+public class VersionComparator implements Comparator {
     @Override
-    public int compare(String object1, String object2) {
-        Matcher m = p.matcher(object1);
-        Integer number1 = null;
-        if (!m.find()) {
-            Matcher m1 = p.matcher(object2);
-            if (m1.find()) {
-                return object2.compareTo(object1);
-            } else {
-                return object1.compareTo(object2);
-            }
-        } else {
-            Integer number2 = null;
-            number1 = Integer.parseInt(m.group());
-            m = p.matcher(object2);
-            if (!m.find()) {
-                Matcher m1 = p.matcher(object1);
-                if (m1.find()) {
-                    return object2.compareTo(object1);
-                } else {
-                    return object1.compareTo(object2);
-                }
-            } else {
-                number2 = Integer.parseInt(m.group());
-                int comparison = number1.compareTo(number2);
-                if (comparison != 0) {
-                    return comparison;
-                } else {
-                    return object1.compareTo(object2);
-                }
-            }
-        }
-    }
+    public int compare(Object o1, Object o2) {
+        String version1 = (String) o1;
+        String version2 = (String) o2;
 
+        VersionTokenizer tokenizer1 = new VersionTokenizer(version1);
+        VersionTokenizer tokenizer2 = new VersionTokenizer(version2);
+
+        int number1 = 0;
+        int number2 = 0;
+        String suffix1 = "";
+        String suffix2 = "";
+
+        while (tokenizer1.moveNext()) {
+            if (!tokenizer2.moveNext()) {
+                do {
+                    number1 = tokenizer1.getNumber();
+                    suffix1 = tokenizer1.getSuffix();
+                    if (number1 != 0 || suffix1.length() != 0) {
+                        // Version one is longer than number two, and non-zero
+                        return 1;
+                    }
+                }
+                while (tokenizer1.moveNext());
+
+                // Version one is longer than version two, but zero
+                return 0;
+            }
+
+            number1 = tokenizer1.getNumber();
+            suffix1 = tokenizer1.getSuffix();
+            number2 = tokenizer2.getNumber();
+            suffix2 = tokenizer2.getSuffix();
+
+            if (number1 < number2) {
+                // Number one is less than number two
+                return -1;
+            }
+            if (number1 > number2) {
+                // Number one is greater than number two
+                return 1;
+            }
+
+            boolean empty1 = suffix1.length() == 0;
+            boolean empty2 = suffix2.length() == 0;
+
+            if (empty1 && empty2) continue; // No suffixes
+            if (empty1) return 1; // First suffix is empty (1.2 > 1.2b)
+            if (empty2) return -1; // Second suffix is empty (1.2a < 1.2)
+
+            // Lexical comparison of suffixes
+            int result = suffix1.compareTo(suffix2);
+            if (result != 0) return result;
+
+        }
+        if (tokenizer2.moveNext()) {
+            do {
+                number2 = tokenizer2.getNumber();
+                suffix2 = tokenizer2.getSuffix();
+                if (number2 != 0 || suffix2.length() != 0) {
+                    // Version one is longer than version two, and non-zero
+                    return -1;
+                }
+            }
+            while (tokenizer2.moveNext());
+
+            // Version two is longer than version one, but zero
+            return 0;
+        }
+        return 0;
+    }
 }
