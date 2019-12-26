a2c4c8c28f28766621e0870746bfb1b3cce3d278
Ramesh Babu Prudhvi <rprudhvi@outlook.com>
2019-12-26 23:19:21 +0530

    [Build] Added selcukes-core dependency

--- src/test/java/io/github/selcukes/tests/WebDriverBinaryTest.java
+++ src/test/java/io/github/selcukes/tests/WebDriverBinaryTest.java
@@ -1,54 +1,63 @@
 package io.github.selcukes.tests;
 
 
+import io.github.selcukes.logging.Logger;
+import io.github.selcukes.logging.LoggerFactory;
 import io.github.selcukes.wdb.WebDriverBinary;
-
-
-import java.util.logging.Logger;
-
 import org.testng.annotations.Test;
 
 import static java.lang.System.getProperty;
-
 import static org.testng.Assert.assertEquals;
 
 
 public class WebDriverBinaryTest {
-    private Logger logger = Logger.getLogger(WebDriverBinaryTest.class.getName());
+    private Logger logger = LoggerFactory.getLogger(WebDriverBinaryTest.class);
 
     @Test
-    public void downloadBinaryTest() {
+    public void chromeDriverTest() {
         String binProp = WebDriverBinary.chromeDriver().targetPath("temp").setup();
         assertEquals("webdriver.chrome.driver", binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + getProperty(binProp) + "}");
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + getProperty(binProp) + "}");
+    }
 
-        binProp = WebDriverBinary.ieDriver().targetPath("temp").arch32().setup();
-        assertEquals("webdriver.ie.driver", binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + getProperty(binProp) + "}");
-
-        binProp = WebDriverBinary.firefoxDriver().targetPath("temp").version("v0.26.0").arch64().setup();
+    @Test
+    public void firefoxDriverTest() {
+        String binProp = WebDriverBinary.firefoxDriver().targetPath("temp").version("v0.26.0").arch64().setup();
         assertEquals("webdriver.gecko.driver", binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + getProperty(binProp) + "}");
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + getProperty(binProp) + "}");
+    }
 
-        binProp = WebDriverBinary.edgeDriver().targetPath("temp").setup();
+    @Test
+    public void ieDriverTest() {
+        String binProp = WebDriverBinary.ieDriver().targetPath("temp").arch32().setup();
+        assertEquals("webdriver.ie.driver", binProp);
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + getProperty(binProp) + "}");
+    }
+
+    @Test
+    public void edgeDriverTest() {
+
+        String binProp = WebDriverBinary.edgeDriver().targetPath("temp").setup();
         assertEquals("webdriver.edge.driver", binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + getProperty(binProp) + "}");
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + getProperty(binProp) + "}");
+    }
 
-        binProp = WebDriverBinary.grid().targetPath("temp").setup();
+    @Test
+    public void seleniumServerTest() {
+        String binProp = WebDriverBinary.grid().targetPath("temp").setup();
         assertEquals("webdriver.grid.driver", binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + getProperty(binProp) + "}");
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + getProperty(binProp) + "}");
     }
 
     @Test
     public void reuseDownloadedBinaryTest() {
         String binProp = WebDriverBinary.chromeDriver().setup();
         String binaryDownloadedPath = getProperty(binProp);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + binaryDownloadedPath + "}");
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + binaryDownloadedPath + "}");
 
         String binProp1 = WebDriverBinary.chromeDriver().setup();
         String binaryDownloadedPath1 = getProperty(binProp1);
-        logger.info("Path set for the Property { " + binProp + "} from the location {" + binaryDownloadedPath1 + "}");
-
+        logger.debug(() -> "Binary path for { " + binProp + "} is {" + binaryDownloadedPath1 + "}");
         assertEquals(binaryDownloadedPath, binaryDownloadedPath1);
 
     }
