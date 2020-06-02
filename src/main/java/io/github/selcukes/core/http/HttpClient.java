b33e32005810150bdca65deb152985a7da613638
Ramesh Babu <rprudhvi@outlook.com>
2020-06-02 11:45:34 +0530

    Http Get actions

--- src/main/java/io/github/selcukes/core/http/HttpClient.java
+++ src/main/java/io/github/selcukes/core/http/HttpClient.java
@@ -24,6 +24,8 @@ import io.github.selcukes.core.exception.SelcukesException;
 import io.github.selcukes.core.logging.Logger;
 import io.github.selcukes.core.logging.LoggerFactory;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
@@ -32,7 +34,10 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.net.*;
+import java.util.Optional;
 
 public class HttpClient {
     private final Logger logger = LoggerFactory.getLogger(HttpClient.class);
@@ -41,6 +46,7 @@ public class HttpClient {
     private final ObjectMapper mapper;
     private HttpEntity httpEntity;
     private final String webHookUrl;
+    private String proxy;
 
     public HttpClient(String webHookUrl) {
         this.webHookUrl = webHookUrl;
@@ -73,7 +79,7 @@ public class HttpClient {
         try {
             this.httpEntity = new StringEntity(message);
         } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
+            throw new SelcukesException(e);
         }
         return this.httpEntity;
     }
@@ -102,5 +108,51 @@ public class HttpClient {
             throw new SelcukesException(e);
         }
 
+    }
+
+    private HttpResponse getHttpResponse() {
+        try {
+            URI uri = new URI(webHookUrl);
+            HttpGet request = new HttpGet(webHookUrl);
+            return httpClient.execute(request);
+        } catch (IOException | URISyntaxException e) {
+            throw new SelcukesException(e);
+        }
+
+    }
+
+    private String getHeaderValue(String headerName) {
+        return getHttpResponse().getFirstHeader(headerName).getValue();
+    }
+
+    private InputStream getResponseStream() {
+        InputStream inputStream;
+        try {
+            this.httpEntity = getHttpResponse().getEntity();
+            inputStream = httpEntity.getContent();
+        } catch (IOException e) {
+            throw new SelcukesException(e);
+        }
+        return inputStream;
+    }
+
+    public Optional<Proxy> isProxy() {
+        Optional<URL> url = getProxyUrl(proxy);
+        if (url.isPresent()) {
+            String proxyHost = url.get().getHost();
+            int proxyPort = url.get().getPort() == -1 ? 80
+                : url.get().getPort();
+            return Optional.of(new Proxy(Proxy.Type.HTTP,
+                new InetSocketAddress(proxyHost, proxyPort)));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<URL> getProxyUrl(String proxy) {
+        try {
+            return Optional.of(new URL(proxy));
+        } catch (MalformedURLException e) {
+            return Optional.empty();
+        }
     }
 }
