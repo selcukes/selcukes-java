ea6d51e115255b03ac031bc6d7eb8f2cd4c2d22a
Ramesh Babu <rprudhvi@outlook.com>
2020-06-21 17:31:21 +0530

    Using Core Logger

--- src/main/java/io/github/selcukes/reports/listeners/DriverEventHandler.java
+++ src/main/java/io/github/selcukes/reports/listeners/DriverEventHandler.java
@@ -18,6 +18,8 @@
 
 package io.github.selcukes.reports.listeners;
 
+import io.github.selcukes.core.logging.Logger;
+import io.github.selcukes.core.logging.LoggerFactory;
 import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
@@ -25,159 +27,158 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverEventListener;
 
-import java.util.logging.Logger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DriverEventHandler implements WebDriverEventListener {
 
-    private static final Logger logger = Logger.getLogger(DriverEventHandler.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(DriverEventHandler.class);
 
     private static String getLocatorFromElement(WebElement element) {
         String str = element.toString();
         Pattern p = Pattern.compile("->\\s(.*)(?=\\])");
         Matcher m = p.matcher(str);
         return m.find() && m.groupCount() > 0
-                ? m.group(1)
-                : str;
+            ? m.group(1)
+            : str;
     }
 
     @Override
     public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysSent) {
-        logger.warning("Changed value of element with " + getLocatorFromElement(element));
+        logger.warn(() -> "Changed value of element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void afterClickOn(WebElement element, WebDriver driver) {
-        logger.warning("Clicked on element with " + getLocatorFromElement(element));
+        logger.warn(() -> "Clicked on element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void afterFindBy(By by, WebElement element, WebDriver driver) {
 
-        logger.warning("Element found using [" + by + "]");
+        logger.warn(() -> "Element found using [" + by + "]");
     }
 
     @Override
     public void afterNavigateBack(WebDriver driver) {
-        logger.warning("Navigated backward...");
+        logger.warn(() -> "Navigated backward...");
     }
 
     @Override
     public void afterNavigateForward(WebDriver driver) {
-        logger.warning("Navigated forward...");
+        logger.warn(() -> "Navigated forward...");
     }
 
     @Override
     public void beforeNavigateRefresh(WebDriver webDriver) {
-        logger.warning("Refreshing the page...");
+        logger.warn(() -> "Refreshing the page...");
     }
 
     @Override
     public void afterNavigateRefresh(WebDriver webDriver) {
-        logger.warning("Page refreshed...");
+        logger.warn(() -> "Page refreshed...");
     }
 
     @Override
     public void afterNavigateTo(String url, WebDriver driver) {
-        logger.warning("Navigated to url [" + url + "]");
+        logger.warn(() -> "Navigated to url [" + url + "]");
     }
 
     @Override
     public void afterScript(String script, WebDriver driver) {
 
-        logger.warning("ran script " + StringUtils.abbreviate(script, 128));
+        logger.warn(() -> "ran script " + StringUtils.abbreviate(script, 128));
     }
 
     @Override
     public void beforeSwitchToWindow(String windowName, WebDriver driver) {
-        logger.warning("Switching to window [" + windowName + "]");
+        logger.warn(() -> "Switching to window [" + windowName + "]");
     }
 
     @Override
     public void afterSwitchToWindow(String windowName, WebDriver driver) {
-        logger.warning("after switch to window " + windowName);
+        logger.warn(() -> "after switch to window " + windowName);
     }
 
     @Override
     public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
-        logger.warning("Change value of element with " + getLocatorFromElement(element));
+        logger.warn(() -> "Change value of element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void beforeClickOn(WebElement element, WebDriver driver) {
-        logger.warning("click element with " + getLocatorFromElement(element));
+        logger.warn(() -> "click element with " + getLocatorFromElement(element));
     }
 
     @Override
     public void beforeFindBy(By by, WebElement element, WebDriver driver) {
-        logger.warning("Finding element by [" + by + "]");
+        logger.warn(() -> "Finding element by [" + by + "]");
     }
 
     @Override
     public void beforeNavigateBack(WebDriver driver) {
-        logger.warning("Navigating back...");
+        logger.warn(() -> "Navigating back...");
     }
 
     @Override
     public void beforeNavigateForward(WebDriver driver) {
-        logger.warning("Navigating forward...");
+        logger.warn(() -> "Navigating forward...");
     }
 
     @Override
     public void beforeNavigateTo(String url, WebDriver driver) {
-        logger.warning("Navigating to [" + url + "]");
+        logger.warn(() -> "Navigating to [" + url + "]");
     }
 
     @Override
     public void beforeScript(String script, WebDriver driver) {
 
-        logger.warning("Executing script [" + StringUtils.abbreviate(script, 512) + "]");
+        logger.warn(() -> "Executing script [" + StringUtils.abbreviate(script, 512) + "]");
     }
 
     @Override
     public void onException(Throwable throwable, WebDriver driver) {
 
-        logger.severe("Exception occurred: " + throwable.getMessage());
+        logger.error(() -> "Exception occurred: " + throwable.getMessage());
     }
 
     @Override
     public <X> void beforeGetScreenshotAs(OutputType<X> outputType) {
-        logger.warning("Capturing screenshot...");
+        logger.warn(() -> "Capturing screenshot...");
     }
 
     @Override
     public <X> void afterGetScreenshotAs(OutputType<X> outputType, X x) {
-        logger.warning("Successfully captured screenshot...");
+        logger.warn(() -> "Successfully captured screenshot...");
     }
 
     @Override
     public void beforeGetText(WebElement webElement, WebDriver webDriver) {
-        logger.warning("Getting text from element...");
+        logger.warn(() -> "Getting text from element...");
     }
 
     @Override
     public void afterGetText(WebElement webElement, WebDriver webDriver, String text) {
-        logger.warning("Got text [" + text + "] from element...");
+        logger.warn(() -> "Got text [" + text + "] from element...");
     }
 
     @Override
     public void beforeAlertAccept(WebDriver webDriver) {
-        logger.warning("Accepting Alert pop-up...");
+        logger.warn(() -> "Accepting Alert pop-up...");
     }
 
     @Override
     public void afterAlertAccept(WebDriver webDriver) {
-        logger.warning("Alert dialog accepted...");
+        logger.warn(() -> "Alert dialog accepted...");
     }
 
     @Override
     public void beforeAlertDismiss(WebDriver webDriver) {
-        logger.warning("Dismissing Alert pop-up...");
+        logger.warn(() -> "Dismissing Alert pop-up...");
     }
 
     @Override
     public void afterAlertDismiss(WebDriver webDriver) {
-        logger.warning("Alert dialog dismissed...");
+        logger.warn(() -> "Alert dialog dismissed...");
     }
 }
--- src/main/java/io/github/selcukes/reports/listeners/SelcukesTestNGListener.java
+++ src/main/java/io/github/selcukes/reports/listeners/SelcukesTestNGListener.java
@@ -26,24 +26,24 @@ import org.testng.ITestListener;
 import org.testng.ITestResult;
 
 public class SelcukesTestNGListener implements ITestListener {
-    private final Logger LOGGER = LoggerFactory.getLogger(SelcukesTestNGListener.class);
+    private final Logger logger = LoggerFactory.getLogger(SelcukesTestNGListener.class);
 
     @Override
     public void onFinish(ITestContext context) {
-        LOGGER.info(() -> "PASSED TEST CASES");
+        logger.info(() -> "PASSED TEST CASES");
         context.getPassedTests()
             .getAllResults()
-            .forEach(result -> LOGGER.info(result::getName));
-        LOGGER.info(() -> "FAILED TEST CASES");
+            .forEach(result -> logger.info(result::getName));
+        logger.info(() -> "FAILED TEST CASES");
         context.getFailedTests()
             .getAllResults()
-            .forEach(result -> LOGGER.info(result::getName));
-        LOGGER.info(() -> "Test completed on: " + context.getEndDate());
+            .forEach(result -> logger.info(result::getName));
+        logger.info(() -> "Test completed on: " + context.getEndDate());
     }
 
     @Override
     public void onStart(ITestContext arg0) {
-        LOGGER.info(() -> "Started testing on: " + arg0.getStartDate());
+        logger.info(() -> "Started testing on: " + arg0.getStartDate());
     }
 
     @Override
@@ -54,26 +54,26 @@ public class SelcukesTestNGListener implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult arg0) {
-        LOGGER.info(() -> "Failed : " + arg0.getName());
+        logger.info(() -> "Failed : " + arg0.getName());
 
     }
 
     @Override
     public void onTestSkipped(ITestResult arg0) {
-        LOGGER.info(() -> "Skipped Test: " + arg0.getName());
+        logger.info(() -> "Skipped Test: " + arg0.getName());
 
     }
 
     @Override
     public void onTestStart(ITestResult arg0) {
-        LOGGER.info(() -> "Testing: " + arg0.getName());
+        logger.info(() -> "Testing: " + arg0.getName());
 
     }
 
     @Override
     public void onTestSuccess(ITestResult arg0) {
         long timeTaken = ((arg0.getEndMillis() - arg0.getStartMillis()));
-        LOGGER.info(() -> "Tested: " + arg0.getName() + " Time taken:" + timeTaken + " ms");
+        logger.info(() -> "Tested: " + arg0.getName() + " Time taken:" + timeTaken + " ms");
 
     }
 
