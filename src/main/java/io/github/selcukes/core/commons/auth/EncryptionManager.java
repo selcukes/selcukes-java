8b12025153e2c7c3dbf5de4b60cd13eea642cda3
Ramesh Babu <rprudhvi@outlook.com>
2020-06-29 21:35:59 +0530

    Updated EncryptionService

---
+++ src/main/java/io/github/selcukes/core/commons/auth/EncryptionManager.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) Ramesh Babu Prudhvi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.selcukes.core.commons.auth;
+
+import io.github.selcukes.core.helper.Preconditions;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.ByteBuffer;
+import java.security.GeneralSecurityException;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+
+public class EncryptionManager {
+    private static final int DEFAULT_LENGTH = 128;
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String ALGORITHM = "AES/GCM/NoPadding";
+
+    public static byte[] encryptData(String key, byte[] data) throws GeneralSecurityException {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] iv = new byte[12];
+        secureRandom.nextBytes(iv);
+        SecretKey secretKey = generateSecretKey(key, iv);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(DEFAULT_LENGTH, iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey, parameterSpec);
+        byte[] encryptedData = cipher.doFinal(data);
+        ByteBuffer byteBuffer = ByteBuffer.allocate(4 + iv.length + encryptedData.length);
+        byteBuffer.putInt(iv.length);
+        byteBuffer.put(iv);
+        byteBuffer.put(encryptedData);
+        return byteBuffer.array();
+    }
+
+
+    public static byte[] decryptData(String key, byte[] encryptedData) throws GeneralSecurityException {
+
+        ByteBuffer byteBuffer = ByteBuffer.wrap(encryptedData);
+        int dataSize = byteBuffer.getInt();
+        Preconditions.checkArgument((dataSize < 12 || dataSize >= 16),
+            "Data size is incorrect. Make sure that the incoming data is an AES encrypted file.");
+        byte[] iv = new byte[dataSize];
+        byteBuffer.get(iv);
+        SecretKey secretKey = generateSecretKey(key, iv);
+        byte[] cipherBytes = new byte[byteBuffer.remaining()];
+        byteBuffer.get(cipherBytes);
+        Cipher cipher = Cipher.getInstance(ALGORITHM);
+        GCMParameterSpec parameterSpec = new GCMParameterSpec(DEFAULT_LENGTH, iv);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, parameterSpec);
+        return cipher.doFinal(cipherBytes);
+
+    }
+
+    public static SecretKey generateSecretKey(String password, byte[] iv) throws GeneralSecurityException {
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), iv, 65536, DEFAULT_LENGTH);
+        SecretKeyFactory secretKeyFactory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1");
+        byte[] key = secretKeyFactory.generateSecret(spec).getEncoded();
+        return new SecretKeySpec(key, KEY_ALGORITHM);
+    }
+}
+
--- src/main/java/io/github/selcukes/core/commons/auth/EncryptionServiceImpl.java
+++ src/main/java/io/github/selcukes/core/commons/auth/EncryptionServiceImpl.java
@@ -17,19 +17,11 @@
 package io.github.selcukes.core.commons.auth;
 
 import io.github.selcukes.core.exception.SelcukesException;
-import io.github.selcukes.core.helper.Preconditions;
 import org.apache.commons.codec.binary.Base64;
 
-import javax.crypto.Cipher;
-import javax.crypto.spec.IvParameterSpec;
-import javax.crypto.spec.SecretKeySpec;
-import java.security.Key;
+import java.security.GeneralSecurityException;
 
 public class EncryptionServiceImpl implements EncryptionService {
-    private static final int DEFAULT_LENGTH = 16;
-    private static final String ALGORITHM = "AES/CBC/PKCS5Padding";
-    private static final String KEY_ALGORITHM = "AES";
-    private static final String PREFIX = "൫:";
 
     @Override
     public String encrypt(String text) {
@@ -43,53 +35,27 @@ public class EncryptionServiceImpl implements EncryptionService {
 
     @Override
     public String encrypt(String cryptoKey, String text) {
-        Key aesKey = null;
-        if (cryptoKey != null && !"".equals(cryptoKey)) {
-            aesKey = generateKey(cryptoKey);
-        }
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, aesKey, getIvParameterSpec());
-            return PREFIX + Base64.encodeBase64String(cipher.doFinal(text.getBytes()));
-        } catch (Exception e) {
+            byte[] payload = EncryptionManager.encryptData(cryptoKey, text.getBytes());
+            return Base64.encodeBase64String(payload);
+        } catch (GeneralSecurityException e) {
             throw new SelcukesException(e);
         }
-
     }
 
     @Override
     public String decrypt(String cryptoKey, String encrypted) {
-        Preconditions.checkArgument(encrypted.startsWith(PREFIX), "Invalid Encrypted key");
-        Key aesKey = null;
-        if (cryptoKey != null && !"".equals(cryptoKey)) {
-            aesKey = generateKey(cryptoKey);
-        }
         try {
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, aesKey, getIvParameterSpec());
-            String encryptedString = encrypted.substring(PREFIX.length());
-            return new String(cipher.doFinal(Base64.decodeBase64(encryptedString)));
+            byte[] textToDecrypt = Base64.decodeBase64(encrypted);
+            return new String(EncryptionManager.decryptData(cryptoKey, textToDecrypt));
         } catch (Exception e) {
             throw new SelcukesException(e);
         }
-    }
-
-    private Key generateKey(String cryptoKey) {
-        StringBuilder keyBuilder = new StringBuilder();
-        keyBuilder.append(cryptoKey);
-        do {
-            keyBuilder.append(cryptoKey);
-        } while (keyBuilder.length() < DEFAULT_LENGTH);
-        byte[] payload = keyBuilder.toString().substring(0, DEFAULT_LENGTH).getBytes();
-        return new SecretKeySpec(payload, KEY_ALGORITHM);
     }
 
     private String getCryptoKey() {
         return System.getProperty("selcukes.crypto.key");
     }
 
-    private IvParameterSpec getIvParameterSpec() {
-        return new IvParameterSpec(new byte[DEFAULT_LENGTH]);
-    }
 
 }
