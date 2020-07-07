62640833bcb7a7aaad772b6f19904ae8ee1c08ed
Ramesh Babu <rprudhvi@outlook.com>
2020-07-07 18:24:11 +0530

    Bump Apache HttpClient to 5.0.1

--- src/main/java/io/github/selcukes/core/http/HttpClient.java
+++ src/main/java/io/github/selcukes/core/http/HttpClient.java
@@ -16,23 +16,123 @@
 
 package io.github.selcukes.core.http;
 
-import org.apache.http.entity.mime.content.FileBody;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.selcukes.core.exception.SelcukesException;
+import io.github.selcukes.core.logging.Logger;
+import io.github.selcukes.core.logging.LoggerFactory;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.config.RequestConfig;
+import org.apache.hc.client5.http.entity.mime.FileBody;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
+import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
+import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.HttpEntity;
+import org.apache.hc.core5.http.HttpHeaders;
+import org.apache.hc.core5.http.config.Registry;
+import org.apache.hc.core5.http.config.RegistryBuilder;
+import org.apache.hc.core5.http.io.entity.StringEntity;
+import org.apache.hc.core5.ssl.SSLContexts;
 
-import java.io.InputStream;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import java.io.IOException;
+import java.net.URL;
+import java.security.GeneralSecurityException;
+import java.util.concurrent.TimeUnit;
 
-public interface HttpClient {
-    HttpClient usingProxy(String proxy);
+import static org.apache.hc.client5.http.config.RequestConfig.custom;
+import static org.apache.hc.client5.http.cookie.StandardCookieSpec.STRICT;
 
-    HttpClient usingUrl(String url);
+class HttpClient {
+    private final Logger logger = LoggerFactory.getLogger(HttpClient.class);
+    private static final String APPLICATION_JSON = "application/json";
+    private static final String HTTPS = "https";
+    private CloseableHttpClient client;
 
-    HttpClient usingAuthorization(String authorization);
 
-    String getHeaderValue(String headerName);
+    public HttpGet createHttpGet(URL url) {
+        HttpGet httpGet = new HttpGet(url.toString());
+        httpGet.addHeader(HttpHeaders.USER_AGENT, "Apache-HttpClient/5.0");
+        httpGet.addHeader(HttpHeaders.ACCEPT_ENCODING, "gzip, deflate, br");
+        httpGet.addHeader(HttpHeaders.CACHE_CONTROL, "max-age=0");
 
-    InputStream getResponseStream();
+        RequestConfig requestConfig = custom().setCookieSpec(STRICT)
+            .setConnectTimeout(2, TimeUnit.SECONDS)
+            .build();
+        httpGet.setConfig(requestConfig);
+        return httpGet;
+    }
 
-    String post(Object payload);
+    private void setConnection(HttpClientBuilder builder) throws GeneralSecurityException {
+        HostnameVerifier allHostsValid = (hostname, session) -> hostname
+            .equalsIgnoreCase(session.getPeerHost());
+        SSLContext sslContext = SSLContexts.custom()
+            .loadTrustMaterial(null, (chain, authType) -> true).build();
+        SSLConnectionSocketFactory socketFactory = new SSLConnectionSocketFactory(
+            sslContext, allHostsValid);
+        Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder
+            .<ConnectionSocketFactory>create().register(HTTPS, socketFactory)
+            .register(HTTPS, new PlainConnectionSocketFactory())
+            .build();
+        PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(
+            socketFactoryRegistry);
+        builder.setConnectionManager(cm);
+    }
 
-    String post(FileBody fileBody);
+    protected HttpClient createClient() {
+        HttpClientBuilder builder = HttpClientBuilder.create()
+            .setConnectionManagerShared(true);
+        try {
+            setConnection(builder);
+        } catch (GeneralSecurityException e) {
+            throw new SelcukesException(e);
+        }
+        client = builder.useSystemProperties().build();
+        return this;
+    }
 
+    protected CloseableHttpResponse execute(ClassicHttpRequest request) {
+        try {
+            return client.execute(request);
+        } catch (IOException e) {
+            throw new SelcukesException(e);
+        }
+    }
+
+    protected HttpEntity createStringEntity(Object payload) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            String message = mapper.writeValueAsString(payload);
+            return new StringEntity(message);
+        } catch (JsonProcessingException e) {
+            throw new SelcukesException(e);
+        }
+    }
+
+    protected HttpEntity createMultipartEntityBuilder(FileBody fileBody) {
+        return MultipartEntityBuilder.create().addPart("file", fileBody).build();
+    }
+
+    protected HttpPost createHttpPost(String url, HttpEntity httpEntity) {
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.setEntity(httpEntity);
+        httpPost.setHeader(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON);
+        return httpPost;
+    }
+
+    protected CloseableHttpResponse execute(HttpPost post) {
+        try {
+            return client.execute(post);
+        } catch (IOException e) {
+            throw new SelcukesException(e);
+        }
+    }
 }
---
+++ src/main/java/io/github/selcukes/core/http/WebClient.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) Ramesh Babu Prudhvi.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.selcukes.core.http;
+
+import io.github.selcukes.core.exception.SelcukesException;
+import org.apache.hc.client5.http.classic.methods.HttpPost;
+import org.apache.hc.client5.http.entity.mime.FileBody;
+import org.apache.hc.core5.http.ClassicHttpRequest;
+import org.apache.hc.core5.http.HttpEntity;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class WebClient {
+    private final HttpClient client;
+    private final URL url;
+
+    public WebClient(String url) {
+        this.url = setURL(url);
+        this.client = new HttpClient();
+
+    }
+
+    private URL setURL(String url) {
+        try {
+            return new URL(url);
+        } catch (MalformedURLException e) {
+            throw new SelcukesException(e);
+        }
+    }
+
+    public InputStream getResponseStream() {
+
+        try {
+            ClassicHttpRequest request = client.createHttpGet(url);
+            return client.createClient()
+                .execute(request)
+                .getEntity()
+                .getContent();
+        } catch (IOException e) {
+            throw new SelcukesException(e);
+        }
+    }
+
+    public InputStream post(Object payload) {
+        try {
+            HttpEntity httpEntity = (payload instanceof FileBody) ?
+                client.createMultipartEntityBuilder((FileBody) payload) : client.createStringEntity(payload);
+            HttpPost post = client.createHttpPost(url.getPath(), httpEntity);
+
+            return client.createClient()
+                .execute(post)
+                .getEntity()
+                .getContent();
+        } catch (IOException e) {
+            throw new SelcukesException(e);
+        }
+
+    }
+
+}
