76ca7b7566a8d379a5095d4c70c847869599f980
Ramesh Babu <rprudhvi@outlook.com>
2022-05-31 22:59:03 +0530

    [Core] updated Test

--- selcukes-core/src/test/java/io/github/selcukes/core/tests/web/WebTest.java
+++ selcukes-core/src/test/java/io/github/selcukes/core/tests/web/WebTest.java
@@ -23,11 +23,11 @@ import io.github.selcukes.core.page.WebPage;
 import io.github.selcukes.wdb.enums.DriverType;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class WebTest {
-    @BeforeMethod
+    @BeforeSuite
     public void beforeClass() {
         GridRunner.startSelenium(DriverType.CHROME);
     }
