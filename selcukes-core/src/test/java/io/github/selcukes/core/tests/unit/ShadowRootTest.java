d47dda1ae1f401bb718d63d95ce0536578bebfca
Ramesh Babu <rprudhvi@outlook.com>
2022-06-18 17:56:29 +0530

    Updated Tests

--- selcukes-core/src/test/java/io/github/selcukes/core/tests/unit/ShadowRootTest.java
+++ selcukes-core/src/test/java/io/github/selcukes/core/tests/unit/ShadowRootTest.java
@@ -16,7 +16,6 @@
 
 package io.github.selcukes.core.tests.unit;
 
-import io.github.selcukes.core.listener.MethodResourceListener;
 import io.github.selcukes.core.page.WebPage;
 import io.github.selcukes.wdb.driver.LocalDriver;
 import io.github.selcukes.wdb.enums.DriverType;
@@ -25,10 +24,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
-@Listeners(MethodResourceListener.class)
+
 public class ShadowRootTest {
     WebPage page;
     WebDriver driver;
@@ -44,6 +42,7 @@ public class ShadowRootTest {
         if (driver != null)
             driver.quit();
     }
+
     @Test
     public void shadowElementTest() {
 
