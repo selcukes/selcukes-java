a3ba26ee191394acbc670f92c0460d5c8c6ea43a
Ramesh Babu <rprudhvi@outlook.com>
2022-07-07 22:36:07 +0530

    [commons] Added ServiceLoaderUtils

---
+++ selcukes-commons/src/main/java/io/github/selcukes/commons/helper/ServiceLoaderUtils.java
@@ -0,0 +1,34 @@
+/*
+ *  Copyright (c) Ramesh Babu Prudhvi.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package io.github.selcukes.commons.helper;
+
+import io.github.selcukes.commons.exception.ConfigurationException;
+import io.github.selcukes.databind.utils.Streams;
+import lombok.experimental.UtilityClass;
+
+import java.util.ServiceLoader;
+
+@UtilityClass
+public class ServiceLoaderUtils {
+    public <T> T load(final Class<T> type, final ClassLoader classLoader) {
+        return Streams.of(ServiceLoader.load(type, classLoader).iterator())
+            .findFirst()
+            .orElseThrow(
+                () -> new ConfigurationException("Could not load {" + type + "}")
+            );
+    }
+}
