ad030d4e02d9f6c49c7c876c580700a50c56c3db
Ramesh Babu <rprudhvi@outlook.com>
2022-09-22 08:47:46 +0530

    [DataBind] Added DateHelper methods in Clocks

--- selcukes-databind/src/main/java/io/github/selcukes/databind/utils/Clocks.java
+++ selcukes-databind/src/main/java/io/github/selcukes/databind/utils/Clocks.java
@@ -18,9 +18,11 @@ package io.github.selcukes.databind.utils;
 
 import lombok.experimental.UtilityClass;
 
+import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
 
 import static java.time.format.DateTimeFormatter.ofPattern;
 import static java.util.Optional.ofNullable;
@@ -29,27 +31,137 @@ import static java.util.Optional.ofNullable;
 public class Clocks {
     public static final String DATE_FORMAT = "yyyy-MM-dd";
     public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String TIMESTAMP_FORMAT = "MM/dd/yyyy hh:mm:ss";
 
-    public String date(final String format) {
-        return LocalDate.now().format(dateTimeFormatter(format, DATE_FORMAT));
+    /**
+     * Return the current date.
+     *
+     * @return The current date.
+     */
+    public LocalDate nowDate() {
+        return LocalDate.now();
     }
 
-    public static String dateTime(final String format) {
-        return LocalDateTime.now().format(dateTimeFormatter(format, DATE_TIME_FORMAT));
+    /**
+     * Return the current date and time.
+     *
+     * @return LocalDateTime.now()
+     */
+    public LocalDateTime nowDateTime() {
+        return LocalDateTime.now();
+    }
+
+    /**
+     * > Returns the current date in the specified format
+     *
+     * @param format The format of the date.
+     * @return A string representation of the current date in the format specified.
+     */
+    public String date(final String format) {
+        return format(nowDate(), format);
+    }
+
+    /**
+     * It takes a date and a format and returns a LocalDate
+     *
+     * @param date The date to be parsed.
+     * @param format The format of the date string.
+     * @return A LocalDate object
+     */
+    public LocalDate dateOf(final String date, final String format) {
+        var dateTimeFormatter = dateTimeFormatter(format, DATE_FORMAT);
+        try {
+            return LocalDate
+                    .parse(date, dateTimeFormatter);
+        } catch (DateTimeException e) {
+            String newFormat = format.isBlank() ? DATE_FORMAT : format;
+            throw new DateTimeException(date + " could not be parsed with '" + newFormat + "' Format");
+        }
+    }
+
+    /**
+     * > Returns the current date and time in the specified format
+     *
+     * @param format The format of the date.
+     * @return A string representation of the current date and time.
+     */
+    public String dateTime(final String format) {
+        return format(nowDateTime(), format);
+    }
+
+    /**
+     * > It takes a date time string and a format string and returns a LocalDateTime object
+     *
+     * @param dateTime The date time string to be parsed.
+     * @param format The format of the dateTime string.
+     * @return A LocalDateTime object
+     */
+    public LocalDateTime dateTimeOf(final String dateTime, final String format) {
+        var dateTimeFormatter = dateTimeFormatter(format, DATE_TIME_FORMAT);
+        try {
+            return LocalDateTime
+                    .parse(dateTime, dateTimeFormatter);
+        } catch (DateTimeException e) {
+            String newFormat = format.isBlank() ? DATE_TIME_FORMAT : format;
+            throw new DateTimeException(dateTime + " could not be parsed with '" + newFormat + "' Format");
+        }
+    }
+
+    /**
+     * > Returns a string representation of the current date and time in the format "yyyy-MM-dd HH:mm:ss.SSS"
+     *
+     * @return A string of the current date and time in the format of "yyyy-MM-dd HH:mm:ss"
+     */
+    public String timeStamp() {
+        return dateTime(TIMESTAMP_FORMAT);
+    }
+
+    /**
+     * It takes a date and a format string, and returns a string representation of the date in the specified format
+     *
+     * @param date The date to be formatted
+     * @param format The format to use for the date.
+     * @return A string representation of the date in the format specified.
+     */
+    public String format(final LocalDate date, final String format) {
+        return date.format(dateTimeFormatter(format, DATE_FORMAT));
+    }
+
+    /**
+     * It takes a date time and a format string, and returns a string representation of the date time in the specified
+     * format
+     *
+     * @param dateTime The date time to format
+     * @param format The format to use for the date.
+     * @return A string representation of the dateTime object.
+     */
+    public String format(final LocalDateTime dateTime, final String format) {
+        return dateTime.format(dateTimeFormatter(format, DATE_TIME_FORMAT));
     }
 
     /**
      * If the format is not null and not empty, return the format, otherwise
      * return the default format.
      *
-     * @param  format        The format to use.
-     * @param  defaultFormat The default format to use if the format parameter
-     *                       is null or empty.
-     * @return               A DateTimeFormatter
+     * @param format        The format to use.
+     * @param defaultFormat The default format to use if the format parameter
+     *                      is null or empty.
+     * @return A DateTimeFormatter
      */
     public static DateTimeFormatter dateTimeFormatter(final String format, final String defaultFormat) {
         return ofPattern(ofNullable(format)
                 .filter(f -> !f.isEmpty())
                 .orElse(defaultFormat));
     }
+
+    /**
+     * Return the last day of the month for the given date.
+     *
+     * @param date The date to adjust
+     * @return The last day of the month.
+     */
+    public LocalDate lastDayOfMonth(final LocalDate date) {
+        return date.with(TemporalAdjusters.lastDayOfMonth());
+    }
+
 }
