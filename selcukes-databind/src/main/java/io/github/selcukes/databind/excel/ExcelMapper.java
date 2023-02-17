c1c36e857e95aa5f841497ee4d3009c68d075138
Ramesh Babu <rprudhvi@outlook.com>
2023-02-17 20:49:32 +0530

    Parse Excel as Map of Maps #165

--- selcukes-databind/src/main/java/io/github/selcukes/databind/excel/ExcelMapper.java
+++ selcukes-databind/src/main/java/io/github/selcukes/databind/excel/ExcelMapper.java
@@ -18,8 +18,21 @@ package io.github.selcukes.databind.excel;
 
 import io.github.selcukes.databind.exception.DataMapperException;
 import io.github.selcukes.databind.utils.DataFileHelper;
+import io.github.selcukes.databind.utils.Streams;
 import lombok.experimental.UtilityClass;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -28,6 +41,9 @@ import java.util.stream.Stream;
  */
 @UtilityClass
 public class ExcelMapper {
+    private static final DataFormatter DATA_FORMATTER = new DataFormatter();
+    private static FormulaEvaluator formulaEvaluator;
+
     /**
      * Parses the Excel file to an Entity Class. It takes a class as input and
      * returns a stream of objects of that class
@@ -47,5 +63,42 @@ public class ExcelMapper {
         }
         ExcelParser<T> excelMapper = new ExcelParser<>(entityClass);
         return excelMapper.parse(dataFile.getPath());
+    }
+
+    /**
+     * It creates a workbook from the file, creates a formula evaluator, and
+     * then creates a map of sheet names to a list of maps of column names to
+     * cell values
+     *
+     * @param  file The file to be parsed.
+     * @return      A map of sheet names to a list of maps of column names to
+     *              cell values.
+     */
+    public Map<String, List<Map<String, String>>> parse(File file) {
+        try (var workbook = WorkbookFactory.create(new FileInputStream(file))) {
+            formulaEvaluator = workbook.getCreationHelper().createFormulaEvaluator();
+            return Streams.of(workbook.iterator())
+                    .collect(Collectors.toMap(Sheet::getSheetName, sheet -> Streams.of(sheet.iterator())
+                            .skip(1)
+                            .map(ExcelMapper::readRow)
+                            .collect(Collectors.toList()),
+                        (k, v) -> k, LinkedHashMap::new));
+        } catch (Exception e) {
+            throw new DataMapperException("Unable to parse Excel file " + file.getAbsolutePath(), e);
+        }
+
+    }
+
+    private Map<String, String> readRow(Row row) {
+        return Streams.of(row.iterator())
+                .collect(Collectors.toMap(cell -> cell.getSheet().getRow(0)
+                        .getCell(cell.getColumnIndex()).getStringCellValue(),
+                    cell -> {
+                        if (cell.getCellType().equals(CellType.FORMULA)) {
+                            return DATA_FORMATTER.formatCellValue(cell, formulaEvaluator);
+                        } else {
+                            return DATA_FORMATTER.formatCellValue(cell);
+                        }
+                    }, (k, v) -> k, LinkedHashMap::new));
     }
 }
