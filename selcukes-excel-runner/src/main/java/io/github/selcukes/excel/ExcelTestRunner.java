be825e5f1b68113d063147554fb782fa40738fa9
Ramesh Babu <rprudhvi@outlook.com>
2023-02-23 10:53:30 +0530

    Cleanup

--- selcukes-excel-runner/src/main/java/io/github/selcukes/excel/ExcelTestRunner.java
+++ selcukes-excel-runner/src/main/java/io/github/selcukes/excel/ExcelTestRunner.java
@@ -23,8 +23,9 @@ import lombok.CustomLog;
 import org.testng.ITestContext;
 import org.testng.annotations.BeforeClass;
 
-import java.util.Optional;
+import java.util.Objects;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static io.github.selcukes.excel.ExcelUtils.NAME_SEPARATOR;
@@ -34,7 +35,7 @@ import static io.github.selcukes.excel.ExcelUtils.runScenarios;
 public class ExcelTestRunner extends SelcukesTestNGRunner {
     @BeforeClass(alwaysRun = true)
     public void setUpExcel(ITestContext context) {
-        SelcukesTestProperties testProperties = new SelcukesTestProperties();
+        var testProperties = new SelcukesTestProperties();
         if (!testProperties.getExcelProperty(SelcukesTestProperties.EXCEL_RUNNER).equalsIgnoreCase("false")) {
             ExcelUtils.initTestRunner();
         }
@@ -42,18 +43,21 @@ public class ExcelTestRunner extends SelcukesTestNGRunner {
 
     @Override
     public Object[][] filter(Object[][] scenarios, Predicate<Pickle> accept) {
-        Object[][] filteredScenarios = super.filter(scenarios, accept);
+        var filteredScenarios = super.filter(scenarios, accept);
         if (runScenarios.isEmpty()) {
             logger.info(() -> "No scenario is selected to execute.");
             return filteredScenarios;
         } else {
+            var scenarioMap = Stream.of(filteredScenarios)
+                    .collect(Collectors.toMap(
+                            s -> s[0].toString().replace("\"", ""),
+                            s -> s,
+                            (s1, s2) -> s1));
             return runScenarios.stream()
                     .map(s -> s.split(NAME_SEPARATOR)[1])
-                    .map(excelScenario -> Stream.of(filteredScenarios)
-                            .filter(scenario -> scenario[0].toString().replace("\"", "")
-                                    .equalsIgnoreCase(excelScenario))
-                            .findAny())
-                    .filter(Optional::isPresent).map(Optional::get).toArray(Object[][]::new);
+                    .map(scenarioMap::get)
+                    .filter(Objects::nonNull)
+                    .toArray(Object[][]::new);
         }
     }
 
