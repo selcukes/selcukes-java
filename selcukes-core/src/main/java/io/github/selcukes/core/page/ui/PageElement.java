a8ab18d6ec274e39403a7d66c9811952e0a4207f
Ramesh Babu <rprudhvi@outlook.com>
2023-03-07 11:08:10 +0530

    [Core] Updated PageElement

--- selcukes-core/src/main/java/io/github/selcukes/core/page/ui/PageElement.java
+++ selcukes-core/src/main/java/io/github/selcukes/core/page/ui/PageElement.java
@@ -23,15 +23,22 @@ import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
+import static java.util.Optional.ofNullable;
+
 @UtilityClass
 public final class PageElement {
     public static final String FILED_ATTRIBUTE_PROPERTY = "selcukes.page.fieldAttribute";
+    public static final String IGNORE_FIELDS_PROPERTY = "selcukes.page.ignoreFields";
     private static final String DEFAULT_FIELD_ATTRIBUTE = "placeholder";
     private static final String FIELD_ATTRIBUTE;
+    private static final List<String> SENSITIVE_FIELDS;
 
     static {
         String attribute = System.getProperty(FILED_ATTRIBUTE_PROPERTY);
         FIELD_ATTRIBUTE = StringHelper.isNullOrEmpty(attribute) ? DEFAULT_FIELD_ATTRIBUTE : attribute;
+        String ignoreLabels = ofNullable(System.getProperty(IGNORE_FIELDS_PROPERTY))
+                .orElse("password");
+        SENSITIVE_FIELDS = List.of(ignoreLabels.split(","));
     }
 
     public String labelName(WebElement element) {
@@ -54,8 +61,7 @@ public final class PageElement {
     }
 
     public String elementValue(String label, String value) {
-        var sensitiveFields = List.of("password");
-        if (StringHelper.containsWord(label, sensitiveFields)) {
+        if (StringHelper.containsWord(label, SENSITIVE_FIELDS)) {
             return "*********";
         } else {
             return value;
