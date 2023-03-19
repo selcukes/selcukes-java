96e7e994b3192ba4ba924428f3008b9f8494155f
Ramesh Babu <rprudhvi@outlook.com>
2023-03-19 10:27:55 +0530

    Update DataBaseDriver

--- selcukes-commons/src/main/java/io/github/selcukes/commons/db/DataBaseDriver.java
+++ selcukes-commons/src/main/java/io/github/selcukes/commons/db/DataBaseDriver.java
@@ -18,6 +18,7 @@ package io.github.selcukes.commons.db;
 
 import io.github.selcukes.commons.exception.SelcukesException;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,20 +26,57 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.concurrent.Executors;
 
+import static java.util.Optional.ofNullable;
+
 /**
- * A driver for a database.
+ * A class representing a driver for a database.
+ * <p>
+ * This class allows executing queries on different types of databases.
  */
+@Getter
 @Builder
 public class DataBaseDriver {
+    /**
+     * The type of database to connect to.
+     */
     private DataBaseType dataBaseType;
+    /**
+     * The hostname or IP address of the database server.
+     */
     private String hostName;
+    /**
+     * The port number to connect to the database.
+     */
     private String port;
+    /**
+     * The username used to authenticate the connection.
+     */
     private String username;
+    /**
+     * The password used to authenticate the connection.
+     */
     private String password;
+    /**
+     * The name of the database to connect to.
+     */
     private String dataBaseName;
+    /**
+     * The timeout to be used for the connection and statement in minutes.
+     */
     private int timeout;
+    /**
+     * The connection URL used to connect to the database.
+     */
+    private String connectionUrl;
 
-    private String getConnectionUrl() {
+    /**
+     * Constructs a connection URL based on the specified database type, host
+     * name, port, and database name.
+     *
+     * @return                   the connection URL as a string.
+     * @throws SelcukesException if an invalid database type is specified.
+     */
+    private String getDefaultConnectionUrl() {
         switch (dataBaseType) {
             case MY_SQL:
                 return "jdbc:mysql://" + hostName + ":" + port + "/" + dataBaseName;
@@ -59,21 +97,40 @@ public class DataBaseDriver {
         }
     }
 
+    /**
+     * Creates a connection to the database using the specified connection URL,
+     * username, and password.
+     *
+     * @return                   a {@link Connection} object.
+     * @throws SelcukesException if an error occurs while creating the
+     *                           connection.
+     */
     private Connection createConnection() {
         Connection connection;
-        var connectionUrl = getConnectionUrl();
+        var dbUrl = ofNullable(getConnectionUrl()).orElse(getDefaultConnectionUrl());
         try {
-            connection = DriverManager.getConnection(connectionUrl, username, password);
+            connection = DriverManager.getConnection(dbUrl, username, password);
             if (!dataBaseType.toString().equals("MY_SQL") && !dataBaseType.toString().equals("POST_GRE_SQL")) {
                 connection.setNetworkTimeout(Executors.newFixedThreadPool(1),
                     timeout * 60000);
             }
         } catch (Exception e) {
-            throw new SelcukesException("Failed to connect DataBase using url[" + connectionUrl + "]");
+            throw new SelcukesException("Failed to connect DataBase using url[" + dbUrl + "]");
         }
         return connection;
     }
 
+    /**
+     * Creates a {@link Statement} object for executing SQL queries on the
+     * specified {@link Connection} object.
+     *
+     * @param  connection        the {@link Connection} object to create the
+     *                           statement on.
+     * @return                   a {@link Statement} object for executing SQL
+     *                           queries.
+     * @throws SelcukesException if there is an error creating the
+     *                           {@link Statement}.
+     */
     private Statement createStatement(final Connection connection) {
         Statement statement;
         try {
@@ -86,17 +143,20 @@ public class DataBaseDriver {
     }
 
     /**
-     * > It creates a connection, creates a statement, executes the query and
-     * returns the result
+     * Executes a query on the database and returns a {@link DataBaseResult}
+     * object.
      *
-     * @param  query The query to be executed.
-     * @return       A DataBaseResult object
+     * @param  query             the query to be executed.
+     * @return                   a {@link DataBaseResult} object.
+     * @throws SelcukesException if an error occurs while executing the query.
      */
     public DataBaseResult executeQuery(final String query) {
-        try {
-            return new DataBaseResult(createStatement(createConnection()).executeQuery(query));
+        try (var connection = createConnection();
+                var statement = createStatement(connection);
+                var resultSet = statement.executeQuery(query)) {
+            return new DataBaseResult(resultSet);
         } catch (Exception e) {
-            throw new SelcukesException("Failed executing query " + query);
+            throw new SelcukesException("Failed executing query " + query, e);
         }
     }
 
