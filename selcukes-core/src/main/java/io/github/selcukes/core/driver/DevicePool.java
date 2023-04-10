dfe4e594b4afbd113c2f20366d50d2283e8730dc
Ramesh Babu <rprudhvi@outlook.com>
2023-04-10 19:17:46 +0530

    Code Cleanup

--- selcukes-core/src/main/java/io/github/selcukes/core/driver/DevicePool.java
+++ selcukes-core/src/main/java/io/github/selcukes/core/driver/DevicePool.java
@@ -24,6 +24,21 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 
+/**
+ * The DevicePool class represents a collection of devices grouped by their
+ * device type.
+ * <p>
+ * Each device is represented as an {@code Object}, which can be a
+ * {@code WebDriver} instance or any other type of device object. The pool
+ * maintains a mapping between the device objects and their device types, and
+ * allows devices to be added, removed, and retrieved based on their device type
+ * and index.
+ * </p>
+ * <p>
+ * The DevicePool class is thread-safe, allowing multiple threads to access and
+ * modify the pool concurrently.
+ * </p>
+ */
 public class DevicePool {
     private final Map<DeviceType, List<Object>> devicesByType;
     private final Map<Object, DeviceType> devicesByObject;
@@ -33,15 +48,46 @@ public class DevicePool {
         devicesByObject = new ConcurrentHashMap<>();
     }
 
+    /**
+     * Adds a device to the device pool for the specified device type.
+     * <p>
+     * The device is represented as an {@code Object}, which can be a
+     * {@code WebDriver} instance or any other type of device object. The device
+     * is added to the end of the list of devices for the specified device type.
+     * </p>
+     * <p>
+     * This method is synchronized to ensure that only one thread can add a
+     * device to the pool at a time.
+     * </p>
+     *
+     * @param deviceType the device type of the device to be added
+     * @param device     the device object to be added
+     */
     public synchronized void addDevice(DeviceType deviceType, Object device) {
         devicesByType.computeIfAbsent(deviceType, k -> new CopyOnWriteArrayList<>()).add(device);
         devicesByObject.put(device, deviceType);
     }
 
+    /**
+     * Returns a list of devices of the given type in the pool.
+     *
+     * @param  deviceType the type of devices being retrieved
+     * @return            a list of devices of the given type in the pool, or an
+     *                    empty list if there are no devices of that type
+     */
     public synchronized List<Object> getDevices(DeviceType deviceType) {
         return devicesByType.getOrDefault(deviceType, Collections.emptyList());
     }
 
+    /**
+     * Returns the device of the given type at the specified index in the pool.
+     *
+     * @param  deviceType                the type of device being retrieved
+     * @param  index                     the index of the device being retrieved
+     * @return                           the device of the given type at the
+     *                                   specified index in the pool
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
     public synchronized Object getDevice(DeviceType deviceType, int index) {
         var deviceList = getDevices(deviceType);
         if (index >= 0 && index < deviceList.size()) {
@@ -52,10 +98,22 @@ public class DevicePool {
         }
     }
 
+    /**
+     * Returns a map of all device types to their corresponding lists of devices
+     * in the pool.
+     *
+     * @return a map of all device types to their corresponding lists of devices
+     *         in the pool
+     */
     public synchronized Map<DeviceType, List<Object>> getAllDevices() {
         return Collections.unmodifiableMap(devicesByType);
     }
 
+    /**
+     * Removes a device from the pool.
+     *
+     * @param device the device being removed
+     */
     public synchronized void removeDevice(Object device) {
         var deviceType = devicesByObject.get(device);
         if (deviceType != null) {
@@ -64,6 +122,15 @@ public class DevicePool {
         }
     }
 
+    /**
+     * Checks if the pool has a device of the given type and with the given
+     * reference.
+     *
+     * @param  deviceType the type of the device being checked
+     * @param  device     the device being checked
+     * @return            true if the pool has a device of the given type and
+     *                    with the given reference, false otherwise
+     */
     public boolean hasDevice(final DeviceType deviceType, final Object device) {
         return getDevices(deviceType).contains(device);
     }
