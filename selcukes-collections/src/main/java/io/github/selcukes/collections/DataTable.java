549d35358c9de2072ba5294bd6b1123f78481c58
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2024-09-05 10:57:30 +0530

    Sonar Fixes

--- selcukes-collections/src/main/java/io/github/selcukes/collections/DataTable.java
+++ selcukes-collections/src/main/java/io/github/selcukes/collections/DataTable.java
@@ -242,7 +242,7 @@ public class DataTable<K, V> extends LinkedList<Map<K, V>> {
         checkColumnIndex(columnName);
         return rows()
                 .map(row -> row.getOrDefault(columnName, null))
-                .toList();
+                .collect(Collectors.toCollection(Lists::of));
     }
 
     /**
