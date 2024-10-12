7041c0a2a636fdccb1d88e3c001ea73c35893c44
Ramesh Babu Prudhvi <rameshbabu.p@yahoo.com>
2024-10-12 19:08:54 +0530

    Formatted

--- selcukes-databind/src/test/java/io/github/selcukes/databind/tests/JsonQueryTest.java
+++ selcukes-databind/src/test/java/io/github/selcukes/databind/tests/JsonQueryTest.java
@@ -1,3 +1,19 @@
+/*
+ *  Copyright (c) Ramesh Babu Prudhvi.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package io.github.selcukes.databind.tests;
 
 import io.github.selcukes.databind.utils.JsonQuery;
@@ -80,7 +96,6 @@ public class JsonQueryTest {
     public void testGetMapValueByPath() {
         JsonQuery jsonParser = new JsonQuery(json);
         var actualBookDetails = jsonParser.get("store.book[3].details", Map.class);
-        System.out.println(actualBookDetails);
         Map<String, Object> expectedBookDetails = Map.of("publisher", "Publisher D",
             "year", 2024);
         Assert.assertEquals(actualBookDetails, expectedBookDetails, "Book details do not match the expected values.");
